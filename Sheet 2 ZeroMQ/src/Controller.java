2c190b3dd733d54bae1dcdb59ca45dbf372aaf60
Henning Klatt <henning.klatt@uni-ulm.de>
2024-11-05 13:47:01 +0100

    factorization works

--- Sheet 2 ZeroMQ/src/Controller.java
+++ Sheet 2 ZeroMQ/src/Controller.java
@@ -18,22 +18,44 @@ public class Controller {
         return "";
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void sendResult(String result){
+        ZContext context = new ZContext();
+        ZMQ.Socket requestSocket = context.createSocket(SocketType.REQ);
+        requestSocket.connect("tcp://vs.lxd-vs.uni-ulm.de:27379");
+        requestSocket.send(result.getBytes(ZMQ.CHARSET), 0);
+        byte[] reply = requestSocket.recv(0);
+        String respone = new String(reply, ZMQ.CHARSET);
+        System.out.println("Received REQ message from publisher: " + respone);
+    }
 
-        var challenge = getChallenge();
+    public static void main(String[] args) throws InterruptedException {
 
         ZContext context = new ZContext();
 
-        // Create PUSH socket for Worker clients
-        ZMQ.Socket socket = context.createSocket(SocketType.PUSH);
-        socket.bind("tcp://*:12345");
+        // Create PUSH socket for worker clients
+        ZMQ.Socket socket_push = context.createSocket(SocketType.PUSH);
+        socket_push.bind("tcp://*:12345");
 
-        while (true) {
+        // Create PULL socket for worker clients
+        ZMQ.Socket socket_pull = context.createSocket(SocketType.PULL);
+        socket_pull.connect("tcp://localhost:12346");
+
+
+        while (!Thread.currentThread().isInterrupted()) {
+            // Get a new factorization challenge from the publisher
+            var challenge = getChallenge();
+
             // Send a message to the workers
-            socket.send(challenge, 0);
-            Thread.sleep(5000);
+            socket_push.send(challenge.getBytes(), 0);
+
+            // Receive the solved factorization from worker
+            String request = socket_pull.recvStr(0);
+            System.out.println("Received PUSH message from worker: " + request);
+
+            // Send solved factorization to publisher
+            sendResult(request);
+
+            Thread.sleep(2000);
         }
-
     }
-
 }
--- Sheet 2 ZeroMQ/src/Worker.java
+++ Sheet 2 ZeroMQ/src/Worker.java
@@ -4,29 +4,33 @@ import org.zeromq.ZMQ;
 
 import java.math.BigInteger;
 
-
 public class Worker {
 
     public static void main(String[] args) {
         ZContext context = new ZContext();
 
-        // Create PULL socket to pull from controller
-        ZMQ.Socket socket = context.createSocket(SocketType.PULL);
-        String host = "tcp://localhost:12345";
-        // Connect to the Controller PUSH socket
-        socket.connect(host);
+        // Create PULL socket to pull challenge from controller
+        ZMQ.Socket socket_pull = context.createSocket(SocketType.PULL);
+        // Connect to the controller PUSH socket
+        socket_pull.connect("tcp://localhost:12345");
 
-        System.out.println("Connected to " + host);
+        // Create PUSH socket to send result back to controller
+        ZMQ.Socket socket_push = context.createSocket(SocketType.PUSH);
+        // Connect to the controller PULL socket
+        socket_push.bind("tcp://*:12346");
+
+        System.out.println("Connected");
 
         while(!Thread.currentThread().isInterrupted()){
-            // wait for next request from the Controller
-            String request = socket.recvStr(0);
-            System.out.println("Received message: " + new String(request));
+            // wait for next request from the controller
+            String request = socket_pull.recvStr(0);
+            System.out.println("Received PULL message from controller: " + new String(request));
 
             BigInteger[] result = Fermat.fermatFactorization(new BigInteger(request));
             System.out.println(request + " = " + result[0] + " * " + result[1]);
 
+            //Send result back to controller via PUSH socket
+            socket_push.send((request + ":" + result[0] + ":" + result[1]).getBytes());
         }
     }
-
 }
