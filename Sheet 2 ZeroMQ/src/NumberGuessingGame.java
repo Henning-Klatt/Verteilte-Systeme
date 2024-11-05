acd701d7e5d5e5f1e1e51f7a3bc77384720c15b3
Henning Klatt <henning.klatt@uni-ulm.de>
2024-11-05 12:18:36 +0100

    take current attempt into consideration

--- Sheet 2 ZeroMQ/src/NumberGuessingGame.java
+++ Sheet 2 ZeroMQ/src/NumberGuessingGame.java
@@ -2,16 +2,10 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class NumberGuessingGame {
-
-    enum Result {
-        TOO_SMALL,
-        TOO_LARGE,
-        CORRECT,
-        NEW_ID
-    }
 
     public static String getCurrentGameID(ZMQ.Socket socket) {
 
@@ -27,39 +21,13 @@ public class NumberGuessingGame {
                 .orElse(null);
     }
 
-    public static Result sendGuess(ZMQ.Socket socket, String gameID, Long guess) {
-        String request = (gameID + ":" + guess);
-        //System.out.println("Request: [" + request + "]");
-        socket.send(request.getBytes(ZMQ.CHARSET), 0);
-        byte[] reply = socket.recv(0);
-        String string = new String(reply, ZMQ.CHARSET);
-        //System.out.println("Received: [" + string + "]");
-
-        if(string.contains("too small")){
-            return Result.TOO_SMALL;
-        }
-        else if(string.contains("too large")){
-            return Result.TOO_LARGE;
-        }
-        else if(string.contains("Correct guess after")){
-            System.out.println("Received: [" + string + "]");
-            Pattern pattern = Pattern.compile("The new gameID is (\\d+)");
-            gameID = pattern.matcher(string)
-                    .results()
-                    .map(mr -> mr.group(1))
-                    .findFirst()
-                    .orElse(null);
-            System.out.println("New gameID: " + gameID);
-            return Result.CORRECT;
-        }
-        else if(string.contains("GameID unknown!")){
-            return Result.NEW_ID;
-        }
-        else if(string.contains("This number has been guessed already.")){
-            System.out.println("Received: [" + string + "]");
-            return Result.NEW_ID;
-        }
-        return null;
+    public static int getAttempt(String response){
+        Pattern pattern = Pattern.compile("Attempt: (\\d+) - ");
+        return Integer.parseInt(Objects.requireNonNull(pattern.matcher(response)
+                .results()
+                .map(mr -> mr.group(1))
+                .findFirst()
+                .orElse(null)));
     }
 
     public static void main(String[] args){
@@ -77,27 +45,46 @@ public class NumberGuessingGame {
         long lowerBound = 1;
         long upperBound = (long) Math.pow(2, 63)-1;
 
-        mainloop:
         while (true){
             long guess = lowerBound + (upperBound - lowerBound) / 2;
-            var result = sendGuess(requestSocket, currentGameID, guess);
-            switch(result){
-                case TOO_SMALL:
-                    lowerBound = guess + 3;
-                    break;
-                case TOO_LARGE:
-                    upperBound = guess + 1;
-                    break;
-                case CORRECT:
-                    currentGameID = getCurrentGameID(requestSocket);
-                    lowerBound = 1;
-                    upperBound = (long) Math.pow(2, 63)-1;
-                    break;// mainloop;
-                default:
-                    currentGameID = getCurrentGameID(requestSocket);
-                    lowerBound = 1;
-                    upperBound = (long) Math.pow(2, 63)-1;
-                    break;
+            String request = (currentGameID + ":" + guess);
+            //System.out.println("Request: [" + request + "]");
+            requestSocket.send(request.getBytes(ZMQ.CHARSET), 0);
+            byte[] reply = requestSocket.recv(0);
+            String string = new String(reply, ZMQ.CHARSET);
+            //System.out.println("Received: [" + string + "]");
+
+            if(string.contains("too small")){
+                var attempt = getAttempt(string);
+                //System.out.println("Received: [" + string + "]");
+                lowerBound = guess + attempt;
+            }
+            else if(string.contains("too large")){
+                var attempt = getAttempt(string);
+                //System.out.println("Received: [" + string + "]");
+                upperBound = guess + 1;
+            }
+            else if(string.contains("Correct guess after")){
+                System.out.println("Received: [" + string + "]");
+                Pattern pattern = Pattern.compile("The new gameID is (\\d+)");
+                currentGameID = pattern.matcher(string)
+                        .results()
+                        .map(mr -> mr.group(1))
+                        .findFirst()
+                        .orElse(null);
+                System.out.println("New gameID: " + currentGameID);
+                lowerBound = 1;
+                upperBound = (long) Math.pow(2, 63)-1;
+            }
+            else if(string.contains("GameID unknown!")){
+                System.out.println("Received: [" + string + "]");
+                currentGameID = getCurrentGameID(requestSocket);
+            }
+            else if(string.contains("This number has been guessed already.")){
+                System.out.println("Received: [" + string + "]");
+                currentGameID = getCurrentGameID(requestSocket);
+                lowerBound = 1;
+                upperBound = (long) Math.pow(2, 63)-1;
             }
         }
     }
