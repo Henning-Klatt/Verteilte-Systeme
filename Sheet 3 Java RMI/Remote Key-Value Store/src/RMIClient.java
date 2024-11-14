4b2c332921ee156313591166c09fd074c0eca5be
Henning Klatt <henning.klatt@uni-ulm.de>
2024-11-14 13:07:48 +0100

    added HashMap functionality

--- Sheet 3 Java RMI/Remote Key-Value Store/src/RMIClient.java
+++ Sheet 3 Java RMI/Remote Key-Value Store/src/RMIClient.java
@@ -1,2 +1,39 @@
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class RMIClient {
+    private RemoteKVStore remoteStore;
+
+    public RMIClient(String hostname, int port) {
+        try {
+            Registry registry = LocateRegistry.getRegistry(hostname, port);
+            remoteStore = (RemoteKVStore) registry.lookup("RMIKVStore");
+        } catch (RemoteException e) {
+            System.err.println("Registry lookup failed: " + e);
+        } catch (NotBoundException e) {
+            System.err.println("Service (RMIKVStore) is not available: " + e);
+        }
+    }
+
+    public void testRemoteStore() {
+        try {
+            remoteStore.writeRemote("key1", "value1");
+            remoteStore.writeRemote("key1", "value2");
+            System.out.println(remoteStore.readRemote("key1"));
+            remoteStore.writeRemote("key2", "value2");
+            remoteStore.removeRemote("key2");
+            System.out.println(remoteStore.readRemote("key2"));
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String hostname = "localhost";
+        RMIClient client = new RMIClient(hostname, 41337);
+
+        client.testRemoteStore();
+    }
 }
