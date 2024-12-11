23055af79619d98f0d11e553e1577902cec359c4
Henning Klatt <henning.klatt@uni-ulm.de>
2024-12-11 19:36:32 +0100

    added ChatClient class

---
+++ Sheet 4 ChatServergRPC/src/main/java/uulm/in/vs/ex4/ChatClient.java
@@ -0,0 +1,13 @@
+package uulm.in.vs.ex4;
+
+public class ChatClient {
+
+    public ChatClient(String host, int port) {
+
+    }
+
+    public static void main(String[] args) {
+        new ChatClient("localhost", 5555);
+    }
+
+}
