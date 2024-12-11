b1394b23ed1fbe1847b37f23ac0fbf6811d76a6e
Henning Klatt <henning.klatt@uni-ulm.de>
2024-12-11 19:18:43 +0100

    added login and logout

--- Sheet 4 ChatServergRPC/src/main/java/uulm/in/vs/ex4/ChatServer.java
+++ Sheet 4 ChatServergRPC/src/main/java/uulm/in/vs/ex4/ChatServer.java
@@ -1,11 +1,13 @@
 package uulm.in.vs.ex4;
 
+import com.google.rpc.context.AttributeContext;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.UUID;
 
 public class ChatServer {
     private final static ConcurrentHashMap<String, String> users = new ConcurrentHashMap<>();
@@ -13,11 +15,38 @@ public class ChatServer {
     public static class ChatService extends ChatGrpc.ChatImplBase {
         @Override
         public void login(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
-            // TODO
+            String username = request.getUsername();
+            LoginResponse.Builder loginResponse = LoginResponse.newBuilder();
+            // Username existiert bereits
+            if (users.containsKey(username)) {
+                System.out.println("User (" + username + ") is already logged in.");
+                loginResponse.setStatus(StatusCode.FAILED);
+            }
+            // Username ist neu
+            else{
+                UUID uuid = UUID.randomUUID();
+                System.out.println("User (" + username + ") with sessionID: " + uuid.toString() + "logged in.");
+                users.put(username, uuid.toString());
+                loginResponse.setStatus(StatusCode.OK);
+                loginResponse.setSessionID(uuid.toString());
+            }
+            responseObserver.onNext(loginResponse.build());
+            responseObserver.onCompleted();
         }
 
         @Override
         public void logout(LogoutRequest request, StreamObserver<LogoutResponse> responseObserver) {
+            LogoutResponse.Builder logoutResponse = LogoutResponse.newBuilder();
+            String username = request.getUsername();
+            // Falls username existiert
+            if (users.containsKey(username)) {
+                System.out.println("User (" + username + ") logged out.");
+                users.remove(username);
+                logoutResponse.setStatus(StatusCode.OK);
+            } else{
+                System.out.println("Logout User (" + username + ") not found.");
+                logoutResponse.setStatus(StatusCode.FAILED);
+            }
             // TODO
         }
 
